45d09963b45ce79983287a39018deda454db7e0c
Bruno Binet <bruno.binet@camptocamp.com>
2011-05-31 11:37:06 +0200

    first import EmptyTestPkg

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='EmptyTestPkg',
+      version=version,
+      description="Empty package for test purpose only",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='empty',
+      author='Bruno Binet',
+      author_email='binet.bruno@gmail.com',
+      url='https://github.com/bbinet/EmptyTestPkg',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
